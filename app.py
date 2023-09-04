d567ee1a36d58b8e91c1366b826b29a210976445
Saad Ahmed <ahmed.saad068@gmail.com>
2023-09-04 20:03:50 +0500

    Changes Made in DeveloperA Branch

--- app.py
+++ app.py
@@ -7,11 +7,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
-
-@app.route("/about")
+@app.route("/About")
 def about():
     return render_template("about.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True,host="0.0.0.0")
